d7eb6d6dde8be3d0d339b692d9c3c24820cd8796
ikjeong <jipark7937@gmail.com>
2023-07-29 22:29:19 +0900

    add client code for grpc communication (for test)

---
+++ cmd/client/main.go
@@ -0,0 +1,46 @@
+/*
+Package client is the client for the gRPC server.
+
+The interfaces are defined in the proto file(api/grpc/metadata.proto).
+
+You can use it when writing the client code.
+
+This code is not used when the command server is operating.
+*/
+package main
+
+import (
+	"context"
+	"flag"
+	grpcapi "github.com/Goboolean/command-server/api/grpc"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func main() {
+
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := grpcapi.NewMetadataServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Login(ctx, &grpcapi.LoginRequest{Id: "test", Password: "test"})
+	if err != nil {
+		log.Fatalf("could not login: %v", err)
+	}
+	log.Printf("Login Token: %s", r.GetUserToken())
+}
