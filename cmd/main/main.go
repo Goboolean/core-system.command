8910eb607fd109e29ac65eff8c96f1b178199273
ikjeong <jipark7937@gmail.com>
2023-07-29 22:28:42 +0900

    add set up grpc server code and rename run.go -> main.go

---
+++ cmd/main/main.go
@@ -0,0 +1,37 @@
+/*
+Package main is the entry point of the command server.
+*/
+package main
+
+import (
+	"context"
+	grpcserver "github.com/Goboolean/command-server/internal/infrastructure/grpc"
+	"log"
+	"os"
+)
+
+func main() {
+
+	// Set up environment variables.
+	err := os.Setenv("COMMAND_SERVER_PORT", "50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set up server.
+	ctx, cancel := context.WithCancel(context.Background())
+	host, err := grpcserver.InitializeHost(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go host.Run(ctx)
+
+	select {
+	case <-ctx.Done():
+		// Every fatel error will be catched here.
+		if err := ctx.Err(); err != nil {
+			log.Fatal(err)
+		}
+		cancel()
+	}
+}
