b83d4de563650f3d3aee5f6d6648ab80283c5618
ikjeong <jipark7937@gmail.com>
2023-10-04 11:23:21 +0900

    add mock gRPC adapter for model

---
+++ internal/adapter/grpc/model/methods.go
@@ -0,0 +1,20 @@
+/*
+Package model provides the implementation of the gRPC methods.
+
+The interfaces are defined in the proto file(api/grpc/model/model.proto).
+
+All commands for services to model management are provided by this adapter.
+*/
+package model
+
+import (
+	"context"
+	"fmt"
+	api "github.com/Goboolean/command-server/api/grpc/model"
+)
+
+func (a *Adapter) RegisterModel(ctx context.Context, in *api.RegisterModelRequest) (nil *api.RegisterModelResponse, err error) {
+	userToken := in.UserToken
+	fmt.Println(userToken)
+	return nil, fmt.Errorf("not implemented")
+}
